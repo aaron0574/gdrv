aa42aa7dcfed137780551dff08ca78aad9f1372f
Aaron Greene <aarongreenedesign@gmail.com>
2020-08-05 23:14:23 -0400

    Massive changes

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,11 @@ $(".access-input").keyup(function () {
     var $next = $(this).next('.access-input');
     if ($next.length)
       $(this).next('.access-input').focus();
+
+      // redirect to dashboard if any numbers added
+      if ($next.length == 0) {
+        location.href = 'video.html';
+      }
     else
       $(this).blur();
   }
